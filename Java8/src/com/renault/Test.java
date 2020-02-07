d2b3f71bebea53d87c2dfc533ea6390655c37b35
Devaraju <devaraju.kommuri@rntbci.com>
2020-02-07 12:02:20 +0530

    DEVA-Configured and First Commit test

--- Java8/src/com/renault/Test.java
+++ Java8/src/com/renault/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// commit check
 		System.out.println("It works");
+		System.out.println("DEVA-checkedout from gilt -> Hello Everyone");
 
 	}
 
